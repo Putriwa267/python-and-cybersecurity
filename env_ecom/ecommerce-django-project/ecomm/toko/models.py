8e4fb55edec1910b1702d3087ea44108e7a87216
Putriwa267 <putriwahyuliana277@gmail.com>
2023-06-02 13:11:51 +0700

    Setting up template

--- env_ecom/ecommerce-django-project/ecomm/toko/models.py
+++ env_ecom/ecommerce-django-project/ecomm/toko/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+PILIHAN_KATEGORI = (
+    ('S', 'Shirt'),
+    ('SW', 'Sport wear'),
+    ('OW', 'Outwear')
+)
+
+PILIHAN_LABEL = (
+    ('NEW', 'primary'),
+    ('SALE', 'info'),
+    ('BEST', 'danger',)
+)
+
+class ProdukItem(models.Model):
+    nama_produk = models.CharField(max_length=100)
+    harga = models.FloatField()
+    harga_diskon = models.FloatField(blank=True, null=True)
+    slug = models.SlugField(unique=True)
+    deskripsi = models.TextField()
+    gambar = models.ImageField(upload_to='product_pic')
+    label = models.CharField(choices=PILIHAN_LABEL, max_length=4)
+    kategori = models.CharField(choices=PILIHAN_KATEGORI, max_length=2)
